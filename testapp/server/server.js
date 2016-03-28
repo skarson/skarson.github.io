e7bc1901c399a7b1f12b31782dbc83de3763f99b
skarson <stevekarson@gmail.com>
2016-03-27 17:37:03 -0700

    stuff

--- testapp/server/server.js
+++ testapp/server/server.js
@@ -4,9 +4,9 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, './../')));
 
-app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname, '../preproduced/preproduced.html'));
-});
+//app.get('/', function(req,res) {
+//  res.sendFile(path.join(__dirname, '../preproduced/preproduced.html'));
+//});
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000');
